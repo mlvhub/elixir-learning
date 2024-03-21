9f03ff22abe947172618c1312a57e6bfb4b1eb8f
Miguel Lopez V <miguellova@gmail.com>
2024-03-21 13:29:56 +0000

    Progress with LV book, page 69

--- programming-phoenix-liveview/pento/lib/pento_web/user_auth.ex
+++ programming-phoenix-liveview/pento/lib/pento_web/user_auth.ex
@@ -173,6 +173,12 @@ defmodule PentoWeb.UserAuth do
   end
 
   defp mount_current_user(socket, session) do
+    IO.puts("mount_current_user")
+
+    Phoenix.Component.assign_new(socket, :session_id, fn ->
+      session["live_socket_id"]
+    end)
+
     Phoenix.Component.assign_new(socket, :current_user, fn ->
       if user_token = session["user_token"] do
         Accounts.get_user_by_session_token(user_token)
